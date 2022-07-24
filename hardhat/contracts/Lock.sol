bb1517fdc1932c6f37d43cfe57a6e6071969b6f8
oleynsve <108637728+oleynsve@users.noreply.github.com>
2022-07-24 19:04:23 +0200

    add the smart contract

--- hardhat/contracts/Lock.sol
+++ hardhat/contracts/Lock.sol
@@ -1,34 +1,47 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+//SPDX-License-Identifier: MIT
 
-// Import this file to use console.log
-import "hardhat/console.sol";
+pragma solidity ^0.8.11;
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-    event Withdrawal(uint amount, uint when);
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+contract hiLo is Ownable {
+    
+    mapping(address => uint256) enteredGame;
+    uint256 public currentGameId;
+    IERC20 public matic;
 
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+  constructor(address _maticTokenAddress) {
+        matic = IERC20(_maticTokenAddress);
+        currentGameId = 0;
     }
 
-    function withdraw() public {
-        // Uncomment this line to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+//===========================================================
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
 
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    // send money from msg.sender to contract
+   function deposit(uint256 amount, uint256 gameId) public payable {
+       require(msg.sender.balance >= amount, "Insufficient Balance");
+        matic.transferFrom(address(msg.sender), address(this), amount);
+        currentGameId = gameId;
+        enteredGame[address(msg.sender)] = currentGameId;
+   }
+ 
+    // send money from contract to recipient
+    function send (address payable recipient, uint256 amount ) public payable onlyOwner  {
+        require(enteredGame[address(recipient)] == currentGameId, "No bets placed for this round");
+        require(address(this).balance >= amount, "Insufficient Balance");
+        require(msg.sender != recipient, "You can't send money to yourself!");
+         matic.transferFrom(address(this), address(recipient), amount);
     }
+
+    function withdraw() external payable onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function getBalance() external view returns (uint256){
+        return address(this).balance;
+    }
+
 }
