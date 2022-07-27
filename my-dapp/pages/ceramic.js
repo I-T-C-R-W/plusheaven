291ddef71845439428ad997506d13d58f616e05a
Ezeoke Godswill <godswillezeoke@gmail.com>
2022-07-27 08:54:31 +0100

    installed dependencies

---
+++ my-dapp/pages/ceramic.js
@@ -0,0 +1,6 @@
+//main.js
+
+import { CeramicClient } from "@ceramicnetwork/http-client";
+import { EthereumAuthProvider } from "@ceramicnetwork/blockchain-utils-linking";
+import { DIDDataStore } from "@glazed/did-datastore";
+import { DIDSession } from "@glazed/did-session";
